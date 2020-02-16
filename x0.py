cd6c5e4af25b0262556cc7374b94cc5fba624d19
vadomerka <pradomiri@gmail.com>
2020-02-16 17:00:03 +0300

    can set X

--- x0.py
+++ x0.py
@@ -4,6 +4,9 @@ lineg = " __ __ __"
 line1 = "{}|{}|{}|{}|"
 lineabc = "  A B C"
 field_names = {}
+fields = {}
+for i in range(1,10):
+    fields[i] = " "
 count = 1
 for i in [1,2,3]:
     for j in ["A","B","C"]:
@@ -12,14 +15,16 @@ for i in [1,2,3]:
 numbers = ['1','2','3','4','5','6','7','8','9']
 while True:
     print(lineabc)
-    print(line1.format(1," "," "," "))
-    print(line1.format(2,"X","X","X"))
-    print(line1.format(3,"X","X"," "))
+    print(line1.format(1,fields[1],fields[2],fields[3]))
+    print(line1.format(2,fields[4],fields[5],fields[6]))
+    print(line1.format(3,fields[7],fields[8],fields[9]))
     number = input("Введите номер ячейки:\n{}\n".format(field_names))
     if number not in numbers:
         print("Введите номер ячейки из диапозона {}".format(numbers))
+        continue
     else:
         number = int(number)
+        fields[number] = "X"
 
 
 
