02698f8588615300a8638f205e663fbedee1414c
vadomerka <pradomiri@gmail.com>
2020-02-16 16:40:47 +0300

    main loop and input number

--- x0.py
+++ x0.py
@@ -9,10 +9,17 @@ for i in [1,2,3]:
     for j in ["A","B","C"]:
         field_names[count] = j + str(i)
         count += 1
-print(lineabc)
-print(line1.format(1,"X","X","X"))
-print(line1.format(2,"X","X","X"))
-print(line1.format(3,"X","X","X"))
+numbers = ['1','2','3','4','5','6','7','8','9']
+while True:
+    print(lineabc)
+    print(line1.format(1," "," "," "))
+    print(line1.format(2,"X","X","X"))
+    print(line1.format(3,"X","X"," "))
+    number = input("Введите номер ячейки:\n{}\n".format(field_names))
+    if number not in numbers:
+        print("Введите номер ячейки из диапозона {}".format(numbers))
+    else:
+        number = int(number)
 
-input("Введите номер ячейки:")
+
 
